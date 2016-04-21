0580db60b3be1a1dbba282e31e10ca59a5412dea
Zac Jones <zacjones@Janes-MacBook-Air.local>
2016-04-21 11:15:32 -0700

    Added reactDOM and updated package

--- app/App.js
+++ app/App.js
@@ -1,8 +1,9 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
 var Router = require('react-router').Router;
 var routes = require('./config/routes');
 
-React.render(
+ReactDOM.render(
   <Router>{routes}</Router>, 
   document.getElementById('app')
 );
